f7653954e007818c37159fef862c83c812115157
goldfinder <36938094+goldfinder@users.noreply.github.com>
2022-12-12 16:09:26 -0500

    Update Gun Test.lua

--- Type 20 Scripting/Gun Test.lua
+++ Type 20 Scripting/Gun Test.lua
@@ -1,36 +1,28 @@
-for _,a in pairs(owner.Character:GetDescendants()) do
-	if a:IsA("Motor6D") then
-		local Weld = Instance.new("Weld")
-		Weld.Part0 = a.Part0
-		Weld.Part1 = a.Part1
-		Weld.C0 = a.C0
-		Weld.C1 = a.C1
-		Weld.Name = a.Name
-		Weld.Parent = a.Parent
-		a:Destroy()
-	end
-end
 --CREDITS
 warn("Model was taken from 'BigToastBoii'.  Handcoded")
 --VARIABLES
 local ammocnt = 31
 local mammocnt = 30
 local mammowadd = 31
-local magcnt = 3*ammocnt
+local FSC = 1
+local magcnt = 3*mammocnt
+local maginstances = {}
 local FLE = false
 local FA = false
 local LE = false
 local LA = false
-local FSM = {}
-FSM.Curr = "Semi"
-FSM.Changeable = true
-FSM.Semi = true
-FSM.FullAuto = true
-FSM.Burst = true
-FSM.GL = false
-FSM.Bolt = false
-FSM.RPG = false
-FSM.Shotgun = false
+local RA = nil
+local LA = nil
+local FSCurr = "Semi"
+FSSettings = {}
+FSSettings.Changeable = true
+FSSettings.Semi = true
+FSSettings.FullAuto = true
+FSSettings.Burst = true
+FSSettings.GL = false
+FSSettings.Bolt = false
+FSSettings.RPG = false
+FSSettings.Shotgun = false
 local boltpos = 0
 GetClick = Instance.new("RemoteEvent")
 GetClick.Parent = owner.Character
@@ -55,43 +47,45 @@ NLS([==[
 game:GetService("UserInputService").InputBegan:Connect(function(key,isTyping)
 	if isTyping == false then
 		local mouse = owner:GetMouse()
-		script.Parent:FireServer(key.KeyCode,"P",mouse.Target,mouse.Hit)
-	end
-end)
-game:GetService("UserInputService").InputEnded:Connect(function(key,isTyping)
-	if isTyping == false then
-		local mouse = owner:GetMouse()
+		script.Parent:FireServer(key.KeyCode,"H",mouse.Target,mouse.Hit)
 		script.Parent:FireServer(key.KeyCode,"R",mouse.Target,mouse.Hit)
-	end
-end)
-game:GetService("UserInputService").InputEnded:Connect(function(key,isTyping)
-	if isTyping == false then
-		local mouse = owner:GetMouse()
 		script.Parent:FireServer(key.KeyCode,"V",mouse.Target,mouse.Hit)
-	end
-end)
-game:GetService("UserInputService").InputEnded:Connect(function(key,isTyping)
-	if isTyping == false then
-		local mouse = owner:GetMouse()
 		script.Parent:FireServer(key.KeyCode,"F",mouse.Target,mouse.Hit)
-	end
-end)
-game:GetService("UserInputService").InputEnded:Connect(function(key,isTyping)
-	if isTyping == false then
-		local mouse = owner:GetMouse()
 		script.Parent:FireServer(key.KeyCode,"M",mouse.Target,mouse.Hit)
 	end
 end)
 ]==],GetKeys)
+for _,a in pairs(owner.Character:GetDescendants()) do
+	if a:IsA("Motor6D") and a.Name ~= "Left Hip" and a.Name ~= "Right Hip" then
+		local Weld = Instance.new("Weld")
+		Weld.Part0 = a.Part0
+		Weld.Part1 = a.Part1
+		Weld.C0 = a.C0
+		Weld.C1 = a.C1
+		Weld.Name = a.Name
+		Weld.Parent = a.Parent
+		if     a.Name == "Right Shoulder" then
+			RA = Weld
+		elseif a.Name == "Left Shoulder" then
+			LA = Weld
+		end
+		a:Destroy()
+	end
+end
 --Weapon load
 local weapon
 local HttpService = game:GetService("HttpService")
-local code = HttpService:GetAsync("https://raw.githubusercontent.com/goldfinder/Gold3nF1r3s-Scripts/main/Type%2020%20Scripting/Model%20Loading", true)
+local code = HttpService:GetAsync("https://raw.githubusercontent.com/goldfinder/Gold3nF1r3s-Scripts/648ce3703491a117ba58eb0ca7f8922a4b586853/Type%2020%20Scripting/Model%20Loading", true)
 weapon = loadstring(code)()
 weapon.Parent = script
 weapon.PrimaryPart = weapon.Lower
 for i,v in pairs(weapon:GetDescendants()) do
 	if v:IsA("Part") and v ~= weapon.PrimaryPart then
+		if v.Parent.Name == "Mag" then 
+			v.Anchored = false
+			continue
+		else
+		end
 		local NewWeld = Instance.new("Weld")
 		NewWeld.Part0 = weapon.PrimaryPart
 		NewWeld.Part1 = v
@@ -104,117 +98,132 @@ end
 weapon.PrimaryPart.CFrame = owner.Character["Right Arm"].CFrame * CFrame.Angles(math.rad(-80),math.rad(90-20),0) * CFrame.new(0,-1.25,0)
 local HandleWeld = Instance.new("Weld")
 HandleWeld.Part0 = owner.Character["Right Arm"]
-HandleWeld.Part1 = weapon['BCM Gunfighter Mod 3 Grip']
-HandleWeld.C0 = CFrame.new(0,-.75,0) * CFrame.Angles(0,math.pi/2,math.rad(-90))
+HandleWeld.Part1 = weapon['Grip']
+HandleWeld.C0 = CFrame.new(-0.25,-0.9,-0.4) * CFrame.Angles(math.rad(-90),math.rad(0),0,0)
 HandleWeld.C1 = CFrame.new(0,0,0)
 HandleWeld.Parent = owner.Character["Right Arm"]
+maginstances.main = weapon:FindFirstChild("Mag")
+maginstances["1"] = weapon:FindFirstChild("Mag").Bullets
+maginstances["2"] = weapon:FindFirstChild("Mag").Mag2
 weapon.PrimaryPart.Anchored = false
+--Weapon Functionality
+function FireSound()
+	local f = coroutine.create(function()
+		local sound = weapon.Grip.Fire:Clone()
+		sound.Playing = true
+		sound.Parent = weapon.Grip
+		wait(sound.TimeLength)
+		sound:Destroy()
+	end)
+	coroutine.resume(f)
+end
+function ClickSound()
+	local f = coroutine.create(function()
+		local sound = weapon.Grip.Click:Clone()
+		sound.Playing = true
+		sound.Parent = weapon.Grip
+		wait(sound.TimeLength)
+		sound:Destroy()
+	end)
+	coroutine.resume(f)
+end
 function Shoot(plr,hit)
+	if ammocnt == 0 then print ("Cannot fire. (No ammo in magazine)") ClickSound() return end
 	if boltpos == 1 then print ("Cannot fire. (Bolt in forwards position)") return end
-	if ammocnt == 0 then print ("Cannot fire. (No ammo in magazine)") return end
 	print("Firing at hit point of mouse")
 	ammocnt -= 1
+	FireSound()
 	if ammocnt == 0 then
 		print("Empty")
 		Bolt("NPA")
 	end
 end
-function Reload()
+function Reload(targ)
 	print("Reloading.")
+	if weapon:FindFirstChild("Mag") ~= nil then
+		local ammodrop = weapon.Mag:clone()
+		ammodrop.Anchored = false
+		ammodrop.CanCollide = true
+		ammodrop:BreakJoints()
+		game.Debris:AddItem(ammodrop,3)
+		weapon:FindFirstChild("Mag").Transparency = 1
+		weapon:FindFirstChild("Mag").Bullets.Transparency = 1
+		weapon:FindFirstChild("Mag").Mag2.Transparency = 1
+		weapon:FindFirstChild("Mag").Transparency = 0
+		weapon:FindFirstChild("Mag").Bullets.Transparency = 0
+		weapon:FindFirstChild("Mag").Mag2.Transparency = 0
+	else
+		return "Error getting mag."
+	end
+	print(targ)
 end
 function FireSelect()
-	if FSM.Changeable == false then return end
-	if FSM.Curr == "Semi" then
-		if FSM.FullAuto == true then
-			FSM.Curr = "Full"
-		elseif FSM.Burst == true then
-			FSM.Curr = "Burs"
-		elseif FSM.GL == true then
-			FSM.Curr = "GL"
-		elseif FSM.Bolt == true then
-			FSM.Curr = "Bolt"
-		elseif FSM.RPG == true then
-			FSM.Curr = "RPG"
-		elseif FSM.Shotgun == true then
-			FSM.Curr = "Shot"
-		else
-			FSM.Changeable = false
+	if FSSettings.Changeable == false then return end
+	ClickSound()
+	local CONTLOOP = false
+	for i=1,6 do
+		if CONTLOOP == true then continue end
+		FSC+=1
+		if FSC == 8 then
+			FSC = 1
 		end
-	elseif FSM.Curr == "Full" then
-		if FSM.Burst == true then
-			FSM.Curr = "Burs"
-		elseif FSM.GL == true then
-			FSM.Curr = "GL"
-		elseif FSM.Bolt == true then
-			FSM.Curr = "Bolt"
-		elseif FSM.RPG == true then
-			FSM.Curr = "RPG"
-		elseif FSM.Shotgun == true then
-			FSM.Curr = "Shot"
-		elseif FSM.Semi == true then
-			FSM.Curr = "Semi"
-		else
-			FSM.Changeable = false
-		end
-	elseif FSM.Curr == "Burs" then
-		if FSM.GL == true then
-			FSM.Curr = "GL"
-		elseif FSM.Bolt == true then
-			FSM.Curr = "Bolt"
-		elseif FSM.RPG == true then
-			FSM.Curr = "RPG"
-		elseif FSM.Shotgun == true then
-			FSM.Curr = "Shot"
-		elseif FSM.Semi == true then
-			FSM.Curr = "Semi"
-		elseif FSM.FullAuto == true then
-			FSM.Curr = "Full"
-		else
-			FSM.Changeable = false
-		end
-	elseif FSM.Curr == "GL" then
-		if FSM.Bolt == true then
-			FSM.Curr = "Bolt"
-		elseif FSM.RPG == true then
-			FSM.Curr = "RPG"
-		elseif FSM.Shotgun == true then
-			FSM.Curr = "Shot"
-		elseif FSM.Semi == true then
-			FSM.Curr = "Semi"
-		elseif FSM.FullAuto == true then
-			FSM.Curr = "Full"
-		elseif FSM.Burst == true then
-			FSM.Curr = "Burs"
-		else
-			FSM.Changeable = false
+		if FSC == 1 and FSSettings.Semi == true then
+			FSCurr="Semi"
+			CONTLOOP = true
+		elseif FSC == 2 and FSSettings.FullAuto == true then
+			FSCurr="Full"
+			CONTLOOP = true
+		elseif FSC == 3 and FSSettings.Burst == true then
+			FSCurr="Burs"
+			CONTLOOP = true
+		elseif FSC == 4 and FSSettings.GL == true then
+			FSCurr="GL"
+			CONTLOOP = true
+		elseif FSC == 5 and FSSettings.Bolt == true then
+			FSCurr="Bolt"
+			CONTLOOP = true
+		elseif FSC == 6 and FSSettings.RPG == true then
+			FSCurr="RPG"
+			CONTLOOP = true
+		elseif FSC == 7 and FSSettings.Shotgun == true then
+			FSCurr="Shot"
+			CONTLOOP = true
 		end
 	end
-	print(FSM.Curr)
+	if CONTLOOP ~= true then
+		print("Unable to change, setting changeable to false.")
+		FSSettings.Changeable = true
+	end
+	print(FSCurr)
 end
 function Bolt(Task)
 	if Task == "NPA" then
 		if boltpos == 0 then
 			print("Bolt forward Empty")
+			weapon.Bolt.BoltBack.Playing=true
 			boltpos = 1
 		else
-			
+
 		end
 	end
 	if Task == "PI" then
 		if boltpos == 0 then
 			print("Bolt Going forward")
+			weapon.Bolt.BoltBack.Playing=true
 			boltpos = 1
 		else
 			print("Bolt Going Back")
+			weapon.Bolt.BoltForward.Playing=true
 			boltpos = 0
 		end
 	end
 	if Task == "PIBR" then
 		if boltpos == 1 then
 			print("Bolt released.")
+			weapon.Bolt.BoltForward.Playing=true
 			boltpos = 0
 		else
-			
+
 		end
 	end
 end
@@ -224,7 +233,10 @@ function MagCheck()
 	print("Returning magazine.")
 end
 --Connect functions to their events
-GetClick.OnServerEvent:Connect(Shoot)
+GetClick.OnServerEvent:Connect(function(plr,extra)
+	print(plr,extra)
+	Shoot(plr)
+end )
 GetKeys.OnServerEvent:Connect(function(plr,key,var,targ,hit)
 	if key == Enum.KeyCode.R and var == "R" then
 		Reload()
@@ -235,16 +247,25 @@ GetKeys.OnServerEvent:Connect(function(plr,key,var,targ,hit)
 			Bolt("PI")
 		end
 		if ammocnt == 0 then
-			print(magcnt)
-			ammocnt = mammocnt
-			magcnt -= 1*mammocnt
-			print(magcnt)
+			print(magcnt,ammocnt)
+			for i=1,30 do
+				if magcnt ~= 0 then
+					ammocnt+=1
+					magcnt-=1
+				end
+			end
+			print(magcnt,ammocnt)
 		else
+			print(magcnt,ammocnt)
 			local curammo = ammocnt
-			ammocnt = mammowadd
-			print(magcnt)
-			magcnt -= math.abs(curammo-ammocnt)
-			print(magcnt)
+			local missing = math.abs(curammo-mammowadd)
+			for i=1,missing do
+				if magcnt ~= 0 then
+					ammocnt+=1
+					magcnt-=1
+				end
+			end
+			print(magcnt,ammocnt)
 		end
 	end
 	if key == Enum.KeyCode.V and var == "V" then
@@ -257,6 +278,7 @@ GetKeys.OnServerEvent:Connect(function(plr,key,var,targ,hit)
 		MagCheck()
 	end
 end)
+--Animations
 local char = owner.Character
 torso = char.Torso
 LS = torso["Left Shoulder"]
@@ -301,23 +323,3 @@ owner.Character.Humanoid.Running:Connect(function(speed)
 	end
 end)
 local Tick = 0
-Run.Stepped:Connect(function()
-	Tick = Tick + 1
-	if running == false then
-		RootJoint.C1 = RootJoint.C1:Lerp(RootJointC1 *CFrame.new(0,0,0.1+-.05 * math.cos(Tick/25)),0.1)
-		RightHip.C1 = RightHip.C1:Lerp(RightHipC1 * CFrame.new(-0.2,-0.1+.05*math.cos(Tick/25),0),0.1)
-		LeftHip.C1 = LeftHip.C1:Lerp(LeftHipC1 * CFrame.new(0.25,-0.1+.05*math.cos(Tick/25),0),0.1)
-		RightHip.C0 = RightHip.C0:Lerp(RightHipC0 * CFrame.Angles(0,math.rad(-10),0),0.1)
-		LeftHip.C0 = LeftHip.C0:Lerp(LeftHipC0 * CFrame.Angles(0,math.rad(15),0),0.1)
-		RightShoulder.C0 = RightShoulder.C0:Lerp(RightShoulderC0 * CFrame.Angles(math.rad(-15),math.rad(27) + .05 *math.cos(Tick/600),math.rad(50)) * CFrame.new(0,0,-0.5),0.1)
-		LeftShoulder.C0 = LeftShoulder.C0:Lerp(LeftShoulderC0 * CFrame.Angles(math.rad(50),0,math.rad(-80)) * CFrame.new(0,-0.7,0),0.1)
-	else
-		RootJoint.C0 = RootJoint.C0:Lerp(RootJointC0 * CFrame.new(0,0,0 + .1 * math.sin(Tick/5)),0.1)
-		RightHip.C0 = RightHip.C0:Lerp(RightHipC0 * CFrame.new(0,0 - 0.2 * math.sin(Tick/20),0) * CFrame.Angles(0,0,math.rad(-25)+math.rad(35)*math.cos(Tick/10)),0.1)
-		LeftHip.C0 = LeftHip.C0:Lerp(LeftHipC0 * CFrame.new(0,0 + 0.2 * math.sin(Tick/20),0) * CFrame.Angles(0,0,math.rad(25)-math.rad(35)*-math.cos(Tick/10)),0.1)
-		RightShoulder.C0 = RightShoulder.C0:Lerp(RightShoulderC0*CFrame.Angles(0,0,math.rad(120)),0.1)
-		LeftShoulder.C0 = LeftShoulder.C0:Lerp(LeftShoulderC0*CFrame.Angles(0,-0.1*math.cos(Tick/10),-0.5*math.cos(Tick/10)),0.1)
-	end
-end)
-
-game:GetService("VoiceChatService")
