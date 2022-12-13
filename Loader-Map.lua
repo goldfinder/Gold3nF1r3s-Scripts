471e74bad275c0a8f26145f4f371b8cf65113e0d
goldfinder <36938094+goldfinder@users.noreply.github.com>
2022-12-13 00:31:05 -0500

    Update Loader-Map.lua

--- Loader-Map.lua
+++ Loader-Map.lua
@@ -4,6 +4,7 @@ function load(link,name)
 	local string = LoadService:GetAsync(link,true)
 	local f = loadstring(string)()
 	print("Loaded")
+	wait(1)
 end
 load("https://raw.githubusercontent.com/goldfinder/Gold3nF1r3s-Scripts/main/Baseplate.lua","Baseplate")
 load("https://raw.githubusercontent.com/goldfinder/Gold3nF1r3s-Scripts/main/Center","Center")
